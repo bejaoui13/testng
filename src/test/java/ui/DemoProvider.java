3f9f5356a6aa0394473441d69f056840e7768c35
QUALITY-4 <QUALITY-4@DESKTOP-TCCOMSR>
2022-07-15 15:24:08 +0100

    v1.1

---
+++ src/test/java/ui/DemoProvider.java
@@ -0,0 +1,37 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoProvider {
+
+	@Test(dataProvider="login")
+	public void testDemo( String username, String password)
+	{
+       WebDriverManager.chromedriver().setup();
+       WebDriver driver = new ChromeDriver();
+       driver.get("https://www.saucedemo.com/");
+       
+       driver.findElement(By.name("user-name")).sendKeys(username);
+       driver.findElement(By.name("password")).sendKeys(password);
+       driver.findElement(By.name("login-button")).click();
+      // driver.close();
+	}
+	
+	@DataProvider(name="login")
+	public Object[][] dataDemo()
+	{
+		return new Object[][]
+				{
+		       	{"standard_user","secret_sauce"},
+		       	{"locked_out_user","secret_sauce"},
+		       	{"problem_user","secret_sauce"},
+		       	{"performance_glitch_user","secret_sauce"},
+				};
+	}
+ }
---
+++ src/test/java/ui/DemoProvider2.java
@@ -0,0 +1,21 @@
+package ui;
+
+import org.testng.annotations.Test;
+
+public class DemoProvider2{
+
+	
+	@Test(dataProvider="login", dataProviderClass =DemoProvider.class)
+	public void test(String username, String password)
+	{
+		System.out.println(username+".."+ password);
+	}
+	
+	@Test(dataProvider="dataset1", dataProviderClass=DataProvidTest.class )
+	public void test1(String username, String password, String test)
+	{
+		System.out.println(username+"==="+ password+"==="+ test);
+	}
+	
+ 
+}
