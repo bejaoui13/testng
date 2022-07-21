7c566c8b585eebfaf6130910c94b8cb59c5d0abc
QUALITY-4 <QUALITY-4@DESKTOP-TCCOMSR>
2022-07-21 14:21:11 +0100

    retry

--- src/test/java/ui/ListenerDemo.java
+++ src/test/java/ui/ListenerDemo.java
@@ -5,10 +5,12 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 
 import common.BaseTest;
+import common.Retry;
+
 
 public class ListenerDemo extends BaseTest {
 
-	@Test
+    @Test(retryAnalyzer = Retry.class)
 	public void lanchBrowser()
 	{
 		driver.get("https://www.ebay.com/");
